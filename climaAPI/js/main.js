e4874415d9ebc9543b3b8a21377a74e4db995fb1
Samuel <samyamaya4@gmail.com>
2023-01-18 13:10:17 -0600

    INICIO PROYECTO API

---
+++ climaAPI/js/main.js
@@ -0,0 +1,5 @@
+console.log("HOLAA")
+
+// fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b6830ef86898875ed0afef8120b7d9b8")
+// .then((response) => response.json())
+// .then((data) => console.log(data));
